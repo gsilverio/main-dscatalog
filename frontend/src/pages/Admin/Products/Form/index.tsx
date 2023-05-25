1f2f039ba419c75dfdb184b0f0d6ac60a25c20de
Guilherme <guilhermegsilverio@gmail.com>
2023-05-25 15:56:55 -0300

    Form buttons styling

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,4 +1,44 @@
+import './styles.css';
+
 const Form = () => {
-  return <h1>TELA FORM</h1>;
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
+        <form action="">
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="product-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="product-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="product-crud-input">
+                <input type="text" className="form-control base-input" />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div>
+                <textarea
+                  name=""
+                  className="form-control base-input h-auto"
+                  rows={10}
+                ></textarea>
+              </div>
+            </div>
+          </div>
+          <div className="product-crud-buttons-container">
+            <button className="btn btn-outline-danger product-crud-button">
+              CANCELAR
+            </button>
+            <button className="btn btn-primary product-crud-button">
+              SALVAR
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default Form;
--- frontend/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontend/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -13,7 +13,7 @@ const ProductCrudCard = ({ product }: Props) => {
       <div className="product-crud-card-top-container">
         <img src={product.imgUrl} alt={product.name} />
       </div>
-      <div className="">
+      <div className="product-crud-card-description">
         <div className="product-crud-card-bottom-container">
           <h6>{product.name}</h6>
           <ProductPrice price={product.price} />
@@ -23,6 +23,14 @@ const ProductCrudCard = ({ product }: Props) => {
             <CategoryBadge name={category.name} key={category.id} />
           ))}
         </div>
+      </div>
+      <div className="product-crud-card-buttons-container">
+        <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first ">
+          EXCLUIR
+        </button>
+        <button className="btn btn-outline-secondary product-crud-card-button">
+          EDITAR
+        </button>
       </div>
     </div>
   );
