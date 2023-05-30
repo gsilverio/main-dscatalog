903f4247ffc18c825280648ed71ea26622980d4e
Guilherme <guilhermegsilverio@gmail.com>
2023-05-30 12:21:38 -0300

    Pagination finished

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,16 +1,40 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import './styles.css';
-const Pagination = () => {
+import ReactPaginate from 'react-paginate';
+
+type Props = {
+  pageCount: number;
+  range: number;
+  onChange?: (pageNumber: number) => void;
+};
+
+const Pagination = ({ pageCount, range, onChange }: Props) => {
   return (
-    <div className="pagination-container">
-      <ArrowIcon className="arrow-previous arrow-inactive" />
-      <div className="pagination-icon active">1</div>
-      <div className="pagination-icon">2</div>
-      <div className="pagination-icon">3</div>
-      <div className="pagination-icon">...</div>
-      <div className="pagination-icon">10</div>
-      <ArrowIcon className="arrow-next arrow-active" />
-    </div>
+    <>
+      <ReactPaginate
+        pageCount={pageCount}
+        pageRangeDisplayed={range}
+        marginPagesDisplayed={1}
+        containerClassName="pagination-container"
+        pageLinkClassName="pagination-icon"
+        breakClassName="pagination-icon"
+        previousClassName="arrow-previous"
+        nextClassName="arrow-next"
+        activeLinkClassName="pagination-link-active"
+        disabledClassName="arrow-inactive"
+        onPageChange={(items) => (onChange ? onChange(items.selected) : {})}
+        previousLabel={
+          <div className="pagination-arrow-container">
+            <ArrowIcon />
+          </div>
+        }
+        nextLabel={
+          <div className="pagination-arrow-container">
+            <ArrowIcon />
+          </div>
+        }
+      />
+    </>
   );
 };
 
--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -7,21 +7,22 @@ import { Product } from 'types/product';
 import { AxiosRequestConfig } from 'axios';
 import { requestBackEnd } from 'util/requests';
 import { get } from 'http';
+import Pagination from 'components/Pagination';
 
 const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
 
   useEffect(() => {
-    getProducts();
+    getProducts(0);
   }, []);
 
-  const getProducts = () => {
+  const getProducts = (pageNumber: number) => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: `/products`,
       params: {
-        page: 0,
-        size: 50,
+        page: pageNumber,
+        size: 3,
       },
       //NAO TEM DATA PORQUE GET NAO PRECISA DE CORPO
     };
@@ -44,10 +45,18 @@ const List = () => {
       <div className="row">
         {page?.content.map((product) => (
           <div className="col-sm-6 col-md-12" key={product.id}>
-            <ProductCrudCard product={product} onDelete={() => getProducts()} />
+            <ProductCrudCard
+              product={product}
+              onDelete={() => getProducts(page.number)}
+            />
           </div>
         ))}
       </div>
+      <Pagination
+        pageCount={page ? page.totalPages : 0}
+        range={3}
+        onChange={getProducts}
+      />
     </div>
   );
 };
