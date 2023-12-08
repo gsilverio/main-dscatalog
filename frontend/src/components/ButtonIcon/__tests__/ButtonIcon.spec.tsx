20076dc8625cfb0e781b9db6a4a67901d07b6908
gsilverio <guilhermegsilverio@gmail.com>
2023-12-08 16:59:24 -0300

    ButtonIcon test

---
+++ frontend/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import ButtonIcon from '../';
+import React from 'react';
+
+test('buttonIcon should render button with given text', () => {
+  const text = 'FAZER LOGIN';
+
+  render(<ButtonIcon text={text} />);
+
+  /*
+  // eslint-disable-next-line testing-library/no-debugging-utils
+  screen.debug();
+  */
+
+  expect(screen.getByText(text)).toBeInTheDocument();
+});
