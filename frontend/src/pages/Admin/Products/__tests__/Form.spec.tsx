04212040ce2c28edd782c5644016c935b56d703c
gsilverio <guilhermegsilverio@gmail.com>
2023-12-13 18:24:08 -0300

    update Form test

--- frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-wait-for-multiple-assertions */
 import { render, screen, waitFor } from '@testing-library/react';
 import Form from '../Form';
 import {
@@ -7,9 +8,10 @@ import {
 import { myHistory } from '../../../../util/history';
 import userEvent from '@testing-library/user-event';
 import selectEvent from 'react-select-event';
-import { server } from './fixtures';
+import { productResponse, server } from './fixtures';
 import { act } from 'react-dom/test-utils';
 import { ToastContainer } from 'react-toastify';
+import exp from 'constants';
 
 beforeAll(() => {
   server.listen();
@@ -37,7 +39,6 @@ describe('Product form create tests', () => {
       </HistoryRouter>
     );
 
-    // eslint-disable-next-line testing-library/no-debugging-utils
     const nameInput = screen.getByTestId('name');
     const priceInput = screen.getByTestId('price');
     const imgUrlInput = screen.getByTestId('imgUrl');
@@ -54,12 +55,10 @@ describe('Product form create tests', () => {
     );
 
     userEvent.type(descriptionInput, 'Computador brabissimo');
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    act(() => {
-      userEvent.click(submitButton);
-    });
-    /*
-    await waitFor(() => {
+
+    userEvent.click(submitButton);
+
+    /*await waitFor(() => {
       const toastElement = screen.getByText('Cadastrado');
       expect(toastElement).toBeInTheDocument();
     });
@@ -82,6 +81,83 @@ describe('Product form create tests', () => {
     await waitFor(() => {
       const messages = screen.getAllByText('Campo Obrigatorio');
       expect(messages).toHaveLength(5);
+    });
+  });
+
+  test('should clear validation messages when filling out the form correctly', async () => {
+    render(
+      <HistoryRouter history={myHistory}>
+        <Form />
+      </HistoryRouter>
+    );
+    const nameInput = screen.getByTestId('name');
+    const priceInput = screen.getByTestId('price');
+    const imgUrlInput = screen.getByTestId('imgUrl');
+    const descriptionInput = screen.getByTestId('description');
+    const categoriesInput = screen.getByLabelText('Categorias');
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const messages = screen.getAllByText('Campo Obrigatorio');
+      expect(messages).toHaveLength(5);
+    });
+
+    await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+    userEvent.type(nameInput, 'Computador');
+    userEvent.type(priceInput, '5000.12');
+    userEvent.type(
+      imgUrlInput,
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHIx_4e1S6rInTaGdJs7_bSBXIH1FcdTFGug&usqp=CAU'
+    );
+    userEvent.type(descriptionInput, 'Computador brabissimo');
+
+    await waitFor(() => {
+      const messsages = screen.queryAllByAltText('Campo Obrigatorio');
+      expect(messsages).toHaveLength(0);
+    });
+  });
+
+  describe('Product form update tests', () => {
+    beforeEach(() => {
+      (useParams as jest.Mock).mockReturnValue({
+        productId: '2',
+      });
+    });
+
+    test('should render form', async () => {
+      render(
+        <HistoryRouter history={myHistory}>
+          <ToastContainer />
+          <Form />
+        </HistoryRouter>
+      );
+
+      await waitFor(() => {
+        const nameInput = screen.getByTestId('name');
+        const priceInput = screen.getByTestId('price');
+        const imgUrlInput = screen.getByTestId('imgUrl');
+        const descriptionInput = screen.getByTestId('description');
+        const formElement = screen.getByTestId('form');
+
+        expect(nameInput).toHaveValue(productResponse.name);
+        expect(priceInput).toHaveValue(String(productResponse.price));
+        expect(imgUrlInput).toHaveValue(productResponse.imgUrl);
+        expect(descriptionInput).toHaveValue(productResponse.description);
+
+        const ids = productResponse.categories.map((x) => String(x.id));
+        expect(formElement).toHaveFormValues({ categories: ids });
+      });
+      const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+      // eslint-disable-next-line testing-library/no-wait-for-side-effects
+      userEvent.click(submitButton);
+      await waitFor(() => {
+        const toastElement = screen.getByText('Cadastrado');
+        expect(toastElement).toBeInTheDocument();
+      });
+      expect(myHistory.location.pathname).toEqual('/admin/products');
     });
   });
 });
